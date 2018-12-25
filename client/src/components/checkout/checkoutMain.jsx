a3b2974f67f2939cf9989b41c65ba7c41ee9c4f2
Jessica <jessicarhodson@gmail.com>
2018-12-25 10:17:16 -0800

    Add static price and prime logo image

--- client/src/components/checkout/checkoutMain.jsx
+++ client/src/components/checkout/checkoutMain.jsx
@@ -2,10 +2,21 @@ import React from 'react';
 import styled from 'styled-components';
 
 const StyledCheckoutBox = styled.div `
-  height: 510px;
-  width: 240px;
+  height: 576px;
+  width: 242px;
   border: 1px solid #ddd;
   border-radius: 4px;
+  padding: 14px 18px;
+  box-sizing: border-box;
+  display: block;
+  position: relative;
+`
+const Price = styled.div `
+  text-rendering: optimizeLegibility;
+  font-size: 17px;
+  line-height: 1.255;
+  color: #B12704;
+  font-family: Arial, sans-serif;
 `
 
 class Checkout extends React.Component {
@@ -17,7 +28,10 @@ class Checkout extends React.Component {
     return (
       <div>
         <img src={'./images/socialmedia.png'} style={{width: '230px'}}></img>
-        <StyledCheckoutBox />
+        <StyledCheckoutBox>
+          <Price>$17.97</Price>
+          <img src={'./images/primelogo.png'} style={{width: '53px'}}></img>
+        </StyledCheckoutBox>
       </div>
     );
   }
