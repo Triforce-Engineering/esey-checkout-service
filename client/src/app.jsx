ccd02e8ebef27fd96e0c3d899069e6eb137fff94
Jessica <jessicarhodson@gmail.com>
2018-12-26 17:54:00 -0800

    Work to resolve version conflicts

--- client/src/app.jsx
+++ client/src/app.jsx
@@ -1,7 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Checkout from './components/checkout/checkoutMain.jsx';
-import Recs from './components/recs/recsMain.jsx';
 
 ReactDOM.render(<Checkout />, document.getElementById('checkout'));
-ReactDOM.render(<Recs />, document.getElementById('recs'));
