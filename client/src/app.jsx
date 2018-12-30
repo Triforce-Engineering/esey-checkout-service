629d0fffc7c297ae91db088742f7ea0e785906ff
Jessica <jessicarhodson@gmail.com>
2018-12-29 19:25:51 -0800

    Change injection element id to match proxy server

--- client/src/app.jsx
+++ client/src/app.jsx
@@ -2,4 +2,4 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Checkout from './components/checkout/checkoutMain.jsx';
 
-ReactDOM.render(<Checkout />, document.getElementById('checkout'));
+ReactDOM.render(<Checkout />, document.getElementById('Checkout'));
