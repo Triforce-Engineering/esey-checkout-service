aa166f687b6478bad869dfa51ce95591b65e6b2b
e553y <esey.iyasu@gmail.com>
2019-01-18 19:33:17 -0800

    add validation and indexes to tables  - stock cannot be neative  - userId and itemId indexed in multicolumn index since they are   queried simultaneously

--- postgresdb/schema.sql
+++ postgresdb/schema.sql
@@ -4,7 +4,7 @@ CREATE TABLE items (
 	name varchar(255) NOT NULL,
 	vendor varchar(255) NOT NULL,
 	price NUMERIC(10,2) NOT NULL,
-	stock int NOT NULL,
+	stock int NOT NULL 	CONSTRAINT non_negative_stock CHECK( stock >= 0 ),
 	
 	PRIMARY KEY (item_id)
 );
@@ -13,5 +13,7 @@ DROP TABLE IF EXISTS carts;
 CREATE TABLE carts (
 	user_id int NOT NULL,
 	item_id int NOT NULL REFERENCES items(item_id),
-	quantity int NOT NULL
+	quantity int NOT NULL,
+
+	PRIMARY KEY(user_id, item_id)
 );
